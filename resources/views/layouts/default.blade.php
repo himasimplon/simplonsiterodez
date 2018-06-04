59a3131fb56ae821423021935c8f6823fbee6cfc
fridakahlo <aj050630@gmail.com>
2018-06-04 11:26:54 +0200

    Ana

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @include('includes.head')
+</head>
+<body>
+    <header class="row">
+        @include('includes.header')
+    </header>
+
+            @yield('content')
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+</body>
+</html>
